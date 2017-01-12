da23d0b42820c69a6577e74a43b4d84903f88a47
inha <inha0421@gmail.com>
2017-01-12 15:02:03 +0900

    슬라이더 틀 구현 + prev/next버튼구현 + 버튼 "on"클래스구현 + on 버튼 클릭시 해당화면이동

--- 39_Autoslider/슬라이더연습/js/custom.js
+++ 39_Autoslider/슬라이더연습/js/custom.js
@@ -1,16 +1,74 @@
 $(document).ready(function(){
 
-//
-//    function list (){
-//        alert("hey!1");
-//        alert("hey!2");
-//        alert("hey!3");
-//        alert("hey!4");
-//    }
-//
-//
-//    list();
-//
+    //전역변수 선언
+
+    var i = $(".btn .on").index(); //on클래스가 붙어있는 애 찾기
+    var wid = $(".slider").width();
+    var len = $(".btn li").length; //()붙이지마!
+
+
+
+    //버튼에 on 클래스처리
+
+    $(".btn >li").click(function(){
+
+        $(".btn >li").removeClass("on");
+        $(this).addClass("on");
+
+    });
+
+
+    //prev 버튼 구현
+
+    $(".prev").click(function(){
+
+        if(i==0){
+            i = len-1;
+        }
+        else{
+            i= i-1;
+        }
+
+        $(".btn li").removeClass("on");
+        $(".btn li").eq(i).addClass("on");
+        $(".panel").animate({"margin-left":-wid*i},500);
+
+    });
+
+
+
+    //next 버튼 구현
+
+        $(".next").click(function(){
+
+            if(i==len-1){
+                i = 0;
+            }
+            else{
+                i = i+1;
+            }
+
+            $(".btn li").removeClass("on");
+            $(".btn li").eq(i).addClass("on");
+            $(".panel").animate({"margin-left":-wid*i},500);
+
+
+        });
+
+
+    //btn 버튼으로 슬라이드 화면 이동
+
+    $(".btn li").click(function(){
+
+        i = $(this).index();
+
+        $(".btn li").removeClass("on");
+        $(".btn li").eq(i).addClass("on");
+        $(".panel").animate({"margin-left":-wid*i},500);
+
+
+    });
+
 
 
 });
