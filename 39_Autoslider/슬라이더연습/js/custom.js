ef04ebaac66b060066fdffe2c4616c988cd93cf9
inha <inha0421@gmail.com>
2017-01-12 14:35:11 +0900

    first commit

---
+++ 39_Autoslider/슬라이더연습/js/custom.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+//
+//    function list (){
+//        alert("hey!1");
+//        alert("hey!2");
+//        alert("hey!3");
+//        alert("hey!4");
+//    }
+//
+//
+//    list();
+//
+
+
+});
+
+
