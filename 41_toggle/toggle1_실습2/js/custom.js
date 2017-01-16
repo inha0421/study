aac8639ca9462de1859def324592b9d07a433726
inha <inha0421@gmail.com>
2017-01-16 13:55:05 +0900

    ul 토글

---
+++ 41_toggle/toggle1_실습2/js/custom.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+    $(".menu .toggle").click(function(){
+
+        $(".menu .toggle").toggleClass("on"); //버튼 활성화
+        $(".box1").toggleClass("on2"); //토굴토굴토구리
+
+    });
+
+});
