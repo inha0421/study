8d4f0db0659bf2e6b6e44736cbbd23701502932a
inha <inha0421@gmail.com>
2017-01-17 14:53:24 +0900

    fixed acodian

--- 42_아코디언/template_실습/js/custom.js
+++ 42_아코디언/template_실습/js/custom.js
@@ -1,3 +1,28 @@
 $(document).ready(function(){
 
+
+
+
+
+    $(".acodi .menu >li span").click(function(){
+
+        var toggle = $(this).hasClass("on");
+
+        if(toggle){
+
+            $(this).siblings("ul").slideUp(500);
+            $(this).removeClass("on");
+
+        }
+
+        else{
+
+            $(this).siblings("ul").slideDown(500);
+            $(this).addClass("on");
+
+        }
+
+    });
+
+
 });
