173bc024a06fcc5ed8b51fdb1d8dd528c78e2e6a
inha <inha0421@gmail.com>
2017-01-17 13:57:28 +0900

    아코디언 실습완성

--- 42_아코디언/template/js/custom.js
+++ 42_아코디언/template/js/custom.js
@@ -1,3 +1,26 @@
 $(document).ready(function(){
 
+
+    $(".acodi .menu >li span").click(function(){
+
+        var toggle = $(this).hasClass("on");
+        //toggle = 클.릭.한 li 가 on 클래스 갖고 있는지 없는지에 대한 변수
+
+        if(toggle){ //on클래스가 있으면(true이면)
+
+            $(this).siblings("ul").slideUp(500);
+            //클릭한 span의 형제 ul가 사라지게 한 후
+
+            $(this).removeClass("on"); //다시 on 클래스 제거
+        }
+
+        else{
+
+            $(this).siblings("ul").slideDown(500);//형제 ul등장 후
+            $(this).addClass("on"); //on 클래스 추가
+        }
+
+    });
+
+
 });
