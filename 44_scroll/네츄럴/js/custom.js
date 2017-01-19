470f1171e70719d8ebbaf1b38828d71442060622
inha <inha0421@gmail.com>
2017-01-19 17:23:49 +0900

    네추럴라이프 완성

--- 44_scroll/네츄럴/js/custom.js
+++ 44_scroll/네츄럴/js/custom.js
@@ -1,3 +1,87 @@
 $(document).ready(function(){
 
+    var hWrap = $(".headerWrap").scrollTop();
+    var rgt = $(".right").offset().top;
+
+
+
+
+
+
+    $(document).scroll(function(){
+
+        var sc = $(this).scrollTop();
+        $(".box").text(sc);
+
+/*
+
+        if(sc > hWrap){
+
+            $(".headerWrap").addClass("fix");
+
+        }
+        else if(sc ==0 ){
+
+            $(".headerWrap").removeClass("fix");
+
+        }
+*/
+
+
+        if(sc >= rgt ){
+
+            $(".right").addClass("fix");
+        }
+
+        else if(sc < rgt ){
+
+            $(".right").removeClass("fix");
+        }
+
+
+    });
+
+    // top 누르면 상위로 이동
+
+
+    $(".top").click(function(){
+
+        var sc = $(document).scrollTop();
+
+        sc = 0;
+
+    });
+
+
+
+
+    //슬라이더
+
+    $(".prev").click(function(){
+
+        $(".panel").animate({"margin-left":"-200%"},500,function(){
+
+            $(".panel li").first().appendTo(".panel");
+            $(".panel").css({"margin-left":"-100%"});
+
+        });
+
+
+    });
+
+
+    $(".next").click(function(){
+
+        $(".panel").animate({"margin-left":"0%"},500,function(){
+
+            $(".panel li").last().prependTo(".panel");
+            $(".panel").css({"margin-left":"-100%"});
+
+        });
+
+
+    });
+
+
+
 });
