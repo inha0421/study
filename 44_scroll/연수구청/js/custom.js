a914d5dacb92e1197bd7f9c71e32521da2661f59
inha <inha0421@gmail.com>
2017-01-19 14:52:15 +0900

    연수구청 탑메뉴바 스크롤 fix  완성

--- 44_scroll/연수구청/js/custom.js
+++ 44_scroll/연수구청/js/custom.js
@@ -1,3 +1,21 @@
 $(document).ready(function(){
 
+
+    $(document).scroll(function(){
+
+        var sc = $(this).scrollTop();
+
+        if(sc > 215){
+            $("header").addClass("fix");
+           }
+
+        else if(sc == 0){
+            $("header").removeClass("fix");
+        }
+
+
+    });
+
+
+
 });
