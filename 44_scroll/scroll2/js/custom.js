741f36a7456abf8ea2a23a0cc391a1f091fc6da8
inha <inha0421@gmail.com>
2017-01-19 16:15:19 +0900

    사이드바 fix완성

--- 44_scroll/scroll2/js/custom.js
+++ 44_scroll/scroll2/js/custom.js
@@ -1,3 +1,35 @@
 $(document).ready(function(){
 
+
+    var top = $(".right").offset().top; //.right의 시작 top 위치
+
+    //offset : html 태그의 시작위치
+
+    //alert(top);
+
+    $(document).scroll(function(){
+
+        var sc = $(this).scrollTop();
+
+        if(sc >= top ){
+
+            $(".right").addClass("fix");
+
+           }
+
+        else if(sc < top ){
+
+            $(".right").removeClass("fix");
+
+           }
+
+
+
+    });
+
+
+
+
+
+
 });
