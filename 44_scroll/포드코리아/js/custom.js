ed2d5d9d264046942f99e8d5793b6761d4475798
inha <inha0421@gmail.com>
2017-01-19 18:21:39 +0900

    끝!!

--- 44_scroll/포드코리아/js/custom.js
+++ 44_scroll/포드코리아/js/custom.js
@@ -1,3 +1,52 @@
 $(document).ready(function(){
 
+    var ex = $(".explore").offset().top;
+
+    $(document).scroll(function(){
+
+
+        var sc = $(this).scrollTop();
+//        var ex = $(".explore").offset().top;
+//        $(".box").text(sc);
+
+        if(sc >= ex){
+
+            $(".explore").addClass("fix");
+        }
+
+        else if(sc < ex){
+
+            $(".explore").removeClass("fix");
+
+        }
+
+    });
+
+
+    //슬라이더
+
+    var i = $(".navi .on").index();
+    var wid = $(".slider").width();
+    var len = $(".navi li").length;
+
+
+    $(".navi li").click(function(){
+
+        $(".navi li").removeClass("on");
+        $(this).addClass("on");
+
+    });
+
+    $(".navi li").click(function(){
+
+        i = $(this).index();
+
+        $(".navi li").removeClass("on");
+        $(".navi li").eq(i).addClass("on");
+
+        $(".panel").animate({"margin-left":-wid *i},500);
+
+    });
+
+
 });
