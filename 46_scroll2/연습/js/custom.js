4c523204b47395f05f274fa4d6b6488e7cb457d3
inha <inha0421@gmail.com>
2017-01-23 18:18:07 +0900

    멀라아아아ㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏㅏ

--- 46_scroll2/연습/js/custom.js
+++ 46_scroll2/연습/js/custom.js
@@ -1,3 +1,76 @@
 $(document).ready(function(){
 
+    var hei = $(window).height();
+    $(".boxWrap >div").height(hei);
+
+
+    $(window).resize(function(){
+
+        var hei = $(window).height();
+        var sc = $(".boxWrap >div").height(hei);
+
+    });
+
+
+    $(".menu li").click(function(){
+
+        hei = $(window).height();
+        var i = $(this).index();
+//        var j = $(".boxWrap >div").length;
+
+        $(".menu li").removeClass("on");
+        $(this).addClass("on");
+
+        $("body").animate({"scrollTop":i*hei});
+
+    });
+
+        $(window).scroll(function(){
+
+        hei = $(window).height();
+        var sc = $(window).scrollTop();
+
+        if(sc >= hei*0 && sc <hei * 1){
+            $(".menu li").removeClass("on");
+            $(".menu li").eq(0).addClass("on");
+        }
+
+        else if(sc >= hei*1 && sc <hei * 2){
+            $(".menu li").removeClass("on");
+            $(".menu li").eq(1).addClass("on");
+        }
+
+        else if(sc >= hei*2 && sc <hei * 3){
+            $(".menu li").removeClass("on");
+            $(".menu li").eq(2).addClass("on");
+        }
+
+        else if(sc >= hei*3){
+            $(".menu li").removeClass("on");
+            $(".menu li").eq(3).addClass("on");
+        }
+
+
+        if(sc == hei*0){
+            $(".logo1").fadeIn(1000);
+        }
+        else if(sc == hei*1){
+            $(".logo2").fadeIn(1000);
+        }
+        else if(sc == hei*2){
+            $(".logo3").fadeIn(1000);
+        }
+        else if(sc == hei*3){
+            $(".logo4").fadeIn(1000);
+        }
+        else{
+            $(".boxWrap >div >a").fadeOut();
+        }
+
+
+
+    });
+
+
+
 });
